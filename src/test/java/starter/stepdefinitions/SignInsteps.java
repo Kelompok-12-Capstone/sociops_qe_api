583dbfc925d03d5d42e95a8f8e37db55f84e5f4a
Andira Mahendra S <84491959+andirams@users.noreply.github.com>
2023-06-26 03:11:51 +0700

    update structure

--- src/test/java/starter/stepdefinitions/SignInsteps.java
+++ src/test/java/starter/stepdefinitions/SignInsteps.java
@@ -1,11 +1,11 @@
-package starter.stepdefiniton;
+package starter.stepdefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
-import starter.user.POST_SIGNIN;
+import starter.java.POST_SIGNIN;
 
 public class SignInsteps {
 
